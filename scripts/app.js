22bc2dccfe5aff72e246b0776c64708b59d08f12
Jyjystudio <jyhad.benyoussef@gmail.com>
2022-04-16 16:02:01 +0200

    first commit

---
+++ scripts/app.js
@@ -0,0 +1,22 @@
+import RecipesApi from './Api/Api.js';
+import RecipeCard from './templates/RecipeCard.js';
+
+class App {
+	constructor() {
+		this.$cardWrapper = document.querySelector('main .recipes-wrapper');
+		this.recipesApi = new RecipesApi('data/recipes.json');
+	}
+
+	async main() {
+		const recipesData = await this.recipesApi.getRecipes();
+		console.log(recipesData);
+
+		recipesData.map(recipe => {
+			const Template = new RecipeCard(recipe);
+			this.$cardWrapper.appendChild(Template.createCard()); 
+		});
+	}
+}
+
+const app = new App();
+app.main();
