dfa0226b3ee95ebb720c38402e8b66457d5b69d7
Jyjystudio <jyhad.benyoussef@gmail.com>
2022-04-17 02:54:00 +0200

    feat : createCard() retourne toutes les recettes formattées

--- scripts/templates/RecipeCard.js
+++ scripts/templates/RecipeCard.js
@@ -1,26 +1,38 @@
 export default class RecipeCard {
-	constructor(card) {
-		this.card = card;
-		this.$wrapper = document.createElement('div');
-		this.$wrapper.classList.add('recipe-card-wrapper');
+	constructor(recipe) {
+		this.recipe = recipe;
+		this.wrapper = document.createElement('div');
+		this.wrapper.classList.add('recipe-card');
 	}
 
 	createCard() {
 
-		const recipeCard = `<div class="recipe-thumbnail center">
-                                <img src="https://picsum.photos/200"  alt="${this.card.name}"/>
+		const ingredients = this.recipe.ingredients.reduce((previousValue, currentValue) => {
+			if(currentValue.quantity && currentValue.unit) {
+				return `${previousValue}<p class='ingredient'>${currentValue.ingredient} : ${currentValue.quantity} ${currentValue.unit}</p>`;
+			} else if(currentValue.quantity) {
+				return `${previousValue}<p class='ingredient'>${currentValue.ingredient} : ${currentValue.quantity}</p>`;
+			}
+			else {
+				return `${previousValue}<p class='ingredient'>${currentValue.ingredient}</p>`;
+			}
+		}, '');
+		
+		const recipeCard = `<div class="recipe-card__thumbnail">
+                                <img src="assets/thumbnail.jpg"  alt="${this.recipe.name}"/>
                             </div>
-                            <h3 class="fs-16 center">${this.card.name}</h3>
-                            <p class="fs-14 center">
-                                <span>${this.card.ingredients}</span>
-                                -
-                                <span>${this.card.description}</span>
-                            </p>`;
+                            <div class="recipe-card__title-time">
+                                <p class="recipe-card__title">${this.recipe.name}</p>
+                                <p class="recipe-card__time"><i class="far fa-clock"></i><span>${this.recipe.time} min</span></p>
+                            </div>
+                            <div class="recipe-card__ingredients-description">
+                                <div class="recipe-card__ingredients-description--ingredients">${ingredients}</div>
+                                <div class="recipe-card__ingredients-description--description">${this.recipe.description}</div>
+                            </div>`;
         
-		this.$wrapper.innerHTML = recipeCard;
-		return this.$wrapper;
+		this.wrapper.innerHTML = recipeCard;
+		return this.wrapper;
+		
 	}
-	get recipe() {
-		return this.card;
-	}
+
 }
