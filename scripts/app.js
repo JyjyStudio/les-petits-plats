2763c4fe2fe1286d269051616f8b2a5d3041cecf
Jyjystudio <jyhad.benyoussef@gmail.com>
2022-05-23 17:39:39 +0200

    rassemblement des fichiers Tag.js et Filter.js

--- scripts/app.js
+++ scripts/app.js
@@ -1,23 +1,20 @@
 import CardFactory from './factories/CardFactory.js';
 import Filter from './utils/Filter.js';
-import Tag from './utils/Tag.js';
 
 class App {
+	
 	constructor() {
 		this.factory = new CardFactory();
 		this.filter = new Filter();
-		this.tag = new Tag();
 	}
 
 	async main() {
-		
-		this.factory.createCards();
-		
-		this.filter;
 
-		this.tag.filterTags();
-		
+		this.factory.createCards();
+		this.filter.filterBy_Searchbar_Tags();
+
 	}
+
 }
 
 const app = new App();
