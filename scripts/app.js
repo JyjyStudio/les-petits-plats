89f13580cadd31038b594a0128942518a3105c5c
Jyjystudio <jyhad.benyoussef@gmail.com>
2022-05-25 13:21:33 +0200

    ajout de la recherche avec la boucle for (filter_B)

--- scripts/app.js
+++ scripts/app.js
@@ -1,17 +1,20 @@
 import CardFactory from './factories/CardFactory.js';
-import Filter from './utils/Filter.js';
+import Filter_A from './utils/Filter.js';
+// import Filter_B from './utils/Filter_B.js';
 
 class App {
 	
 	constructor() {
 		this.factory = new CardFactory();
-		this.filter = new Filter();
+		this.filter_A = new Filter_A();
+		// this.filter_B = new Filter_B();
 	}
 
 	async main() {
 
 		this.factory.createCards();
-		this.filter.filterBy_Searchbar_Tags();
+		this.filter_A.filterBy_Searchbar_Tags();
+		// this.filter_B.filterBy_Searchbar_Tags();
 
 	}
 
